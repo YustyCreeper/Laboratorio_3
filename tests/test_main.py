ad374f8cccd7a7926c76d69b6f0fb3781c964430
YustyCreeper <alessandro,yusty@uao.edu.co>
2025-11-06 19:40:11 -0500

    test main nuevo

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,3 +14,9 @@ def test_read_2_root():
     response = client.get("/hello")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
+
+
+def test_hola_endpoint():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"message": "test endpoint"}
